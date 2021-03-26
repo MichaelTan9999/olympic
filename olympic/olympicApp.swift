7ba9428d01d94cd103eca99e3028bd3594b96bb1
Michael Tan <thr205@hotmail.com>
2021-03-26 22:02:56 +0800

    Initial Commit

---
+++ olympic/olympicApp.swift
@@ -0,0 +1,17 @@
+//
+//  olympicApp.swift
+//  olympic
+//
+//  Created by Michael Tan on 2021/3/26.
+//
+
+import SwiftUI
+
+@main
+struct olympicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
